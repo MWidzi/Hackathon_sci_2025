7d4d8631d10bd26eedaedfcd80868d40a4abaec5
Maciej Widziński <126957101+MWidzi@users.noreply.github.com>
2025-11-22 07:46:19 +0100

    Delete Assets directory

--- Assets/Scripts/Map/CurrentLevel.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CurrentLevel", menuName="Game/CurrentLevel")]
-public class CurrentLevel : ScriptableObject{
-    public int currentLevel;
-}
--- Assets/Scripts/Map/SetLevelToOneOnGameReset.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class SetLevelToOneOnGameRestart : MonoBehaviour{
-    public CurrentLevel currentLevel;
-    void Start(){
-        currentLevel.currentLevel = 1;
-    }
-}
